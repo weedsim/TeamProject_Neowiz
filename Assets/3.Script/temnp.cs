728c903f02d4b6c2fe225c261e7691d285147eda
OldAndSick <sps990311naver.com@gmail.com>
2025-12-05 11:39:43 +0900

    가즈아
    가보자잉

---
+++ Assets/3.Script/temnp.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class temnp : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("ºüºñ»Ç");
+    }
+}
