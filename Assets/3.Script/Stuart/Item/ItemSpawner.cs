811dfba383cea371df457a0c1bb511793a42d65f
OldAndSick <sps990311naver.com@gmail.com>
2025-12-06 01:46:22 +0900

    이거 아이템 스포너까지 만들었다 이거야
    유니티 풀 너무 어렵다 그냥 리스트해서 하는게 훨씬 편할듯 붕들아 고생하지마라 내가 다 맞아보고 하는 말이다

---
+++ Assets/3.Script/Stuart/Item/ItemSpawner.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public GameObject itemPrefab;
+    public Transform player;
+
+    private IObjectPool<GameObject> iPool;
+    private float timer;
+    //딕셔너리 안써서 2개 만들어야 함
+    private void Awake()
+    {
+        //풀초기화
+
+        iPool = new ObjectPool<GameObject>(createFunc: () => { GameObject item = Instantiate(itemPrefab);
+            item.GetComponent<Item>().Setup(iPool);
+            return item;
+        },
+        actionOnGet: (Item) => Item.SetActive(true), 
+        actionOnRelease : (item) => item.SetActive(false), 
+        actionOnDestroy: (item) => Destroy(item), maxSize: 20) ;  
+    }//일단 유니티 풀 이용해보려고 썻는데 핵 어렵긴함
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 5f) //스폰 초시계
+        {
+            timer = 0;
+            SpawnItem();
+        }
+    }
+    private void SpawnItem()
+    {
+        GameObject item = iPool.Get();
+
+        Vector3 randomPos = player.position + (Random.insideUnitSphere * 20f);//주변에서 생성
+        item.transform.position = randomPos;
+    }
+}
