5aa4d86045f8a03f6921496057c49a81dbc624bd
weedsim <109517699+weedsim@users.noreply.github.com>
2025-12-06 13:43:47 +0900

    [Feat] Create RankData Loader

---
+++ Assets/3.Script/Kevin/Data/RankData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RankData
+{
+    public string UserName;
+
+
+}
+
+public class FullRankData
+{
+    public List<RankData> RankDatas;
+}
---
+++ Assets/3.Script/Kevin/Manager/JSONDataLoader.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+
+[System.Serializable]
+public class JSONDataLoader
+{
+    //[SerializeField] private const string _rankDataJsonFilePath = Application.persistentDataPath + "/RankData.json";
+
+
+
+
+
+    //public static RankData RankDataLoader()
+    //{
+
+    //}
+}
