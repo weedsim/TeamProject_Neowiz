f60fb6fd0219548cd26fbbb1bca9e139b9d59c76
대한 <daehan0316@gmail.com>
2025-12-06 11:14:30 +0900

    방구뿡

--- Assets/3.Script/Dave/AddForce.cs
+++ Assets/3.Script/Dave/AddForce.cs
@@ -8,23 +8,44 @@ public class AddForce : MonoBehaviour
 {
     [Header("설정")]
     public GameObject PlayCam;
-    public float Force = 20f;
-    public float Boostfarting_Speed = 1000f;
+    public float Force = 30f;
+    public float Boostfarting_Speed = 100f;
+    public WaitForSeconds FartingCooltime = new WaitForSeconds(2f);
+    public float MaxGauge = 100f;
+    public float CurrentGauge = 0f;
+    public float FartCost = 20f;
+    public float FartRegenSpeed = 1f;
+    public float FartRegenGauge = 2f;
 
-    private Rigidbody rigidbody;
+    private new Rigidbody rigidbody;
     private bool farting = false;
     private Vector3 targetDirection;
+    private bool isfarting = false;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
 
+    private void OnEnable()
+    {
+        CurrentGauge = MaxGauge;
+    }
+
     private void OnMove(InputValue value)
     {
+        if (isfarting) return;
+        Mathf.Clamp(CurrentGauge, 0, MaxGauge);
+        CurrentGauge -= FartCost;
+        if (CurrentGauge <= 0f) // 안전장치
+        {
+            CurrentGauge = 0f;
+        }
+        isfarting = true;
         rigidbody.linearVelocity = Vector3.zero;
         StartCoroutine(Boostfarting_co());
         farting = value.isPressed;
+
         if (farting)
         {
             targetDirection = PlayCam.transform.forward;
@@ -37,21 +58,35 @@ public class AddForce : MonoBehaviour
         {
             rigidbody.AddForce(targetDirection * Force, ForceMode.Force);
         }
+        if (CurrentGauge < MaxGauge)
+        {
+            StartCoroutine(FillFartGauge_co());
+        }
+        else if (CurrentGauge >= MaxGauge)
+        {
+            StopCoroutine(FillFartGauge_co());
+        }
     }
-
     private IEnumerator Boostfarting_co()
     {
         Vector3 DDong = transform.forward;
         rigidbody.linearVelocity = transform.forward * Boostfarting_Speed;
+        yield return FartingCooltime;
         while (rigidbody.linearVelocity.magnitude >= Force)
         {
             rigidbody.linearVelocity -= DDong;
             yield return null;
-            
         }
+        isfarting = false;
     }
 
-
-
-
+    private IEnumerator FillFartGauge_co()
+    {
+        while (true)
+        {
+            if (CurrentGauge >= MaxGauge) break;
+            CurrentGauge += FartRegenGauge;
+            yield return new WaitForSeconds(FartRegenSpeed);
+        }
+    }
 }
