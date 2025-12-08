9247f0e0f78ff09b8cde468880e5241f6cc18d34
대한 <daehan0316@gmail.com>
2025-12-08 11:56:37 +0900

    Mr'Go 춤추게 해보자!!

---
+++ Assets/3.Script/Dave/SelectionToMainGame.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SelectionToMainGame : MonoBehaviour
+{
+    [Header("버튼연결")]
+    public Button SelectionGilr;
+    public Button SelectionBoy;
+    public Button SelectionGo;
+
+
+
+}
