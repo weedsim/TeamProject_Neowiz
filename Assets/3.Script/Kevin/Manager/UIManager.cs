1b89f9b6c0623b27e510ca7cc05d0d7c679daed8
weedsim <109517699+weedsim@users.noreply.github.com>
2025-12-08 16:45:54 +0900

    랭킹 등록 등 랭킹 관련 구성

--- Assets/3.Script/Kevin/Manager/UIManager.cs
+++ Assets/3.Script/Kevin/Manager/UIManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ public class UIManager : MonoBehaviour
     [Header("방귀 게이지 UI")]
     public Image _TargetImage;       // 효과를 줄 이미지
     //public float duration = 0.1f;   // 1프레임에 다시 채워지는 값
+    public Coroutine _Gauge_Co;
 
     [Header("Timer UI")]
     public Text _TimerText;
@@ -19,40 +21,47 @@ public class UIManager : MonoBehaviour
     public int _Sec = 0;
     public int _Min = 0;
     public int _Hour = 0;
+    public Coroutine _Timer_Co;
 
-    private void Awake()
-    {
-        CheckStartTime();
-        GameManager.Instance._Time = 0f;
+    [Header("Video Player")]
+    public GameObject _VideoPlayer;
 
-        if (_TargetImage == null)
-        {
-            GameObject.Find("FartGaugeIMG").TryGetComponent(out _TargetImage);
-        }
+    [Header("GameOver_Canvas")]
+    public Canvas _GameOver_Canvas;
 
-        // 안전장치: 이미지가 연결 되어 있으면 이미지 설정
-        if (_TargetImage != null)
-        {
-            // 코드로 강제 설정 (실수 방지)
-            _TargetImage.type = Image.Type.Filled;
-            _TargetImage.fillMethod = Image.FillMethod.Vertical;
-            _TargetImage.fillOrigin = (int)Image.OriginVertical.Bottom;
-            //targetImage.fillAmount = 1f; // 처음엔 다 보여야 함
-        }
+    //private void Awake()
+    //{
+    //    CheckStartTime();
+    //    GameManager.Instance._Time = 0f;
 
-        if (_player_OBJ == null)
-        {
-            GameObject.FindGameObjectWithTag("Player").TryGetComponent(out _player_OBJ);
-        }
+    //    if (_TargetImage == null)
+    //    {
+    //        GameObject.Find("FartGaugeIMG").TryGetComponent(out _TargetImage);
+    //    }
 
-        if (_TimerText == null)
-        {
-            GameObject.Find("LivingTimer").TryGetComponent(out _TimerText);
-        }
+    //    // 안전장치: 이미지가 연결 되어 있으면 이미지 설정
+    //    if (_TargetImage != null)
+    //    {
+    //        // 코드로 강제 설정 (실수 방지)
+    //        _TargetImage.type = Image.Type.Filled;
+    //        _TargetImage.fillMethod = Image.FillMethod.Vertical;
+    //        _TargetImage.fillOrigin = (int)Image.OriginVertical.Bottom;
+    //        //targetImage.fillAmount = 1f; // 처음엔 다 보여야 함
+    //    }
 
-        StartCoroutine(UpdateTimer_Co());
-        StartCoroutine(UpdateGauge());
-    }
+    //    if (_player_OBJ == null)
+    //    {
+    //        GameObject.FindGameObjectWithTag("Player").TryGetComponent(out _player_OBJ);
+    //    }
+
+    //    if (_TimerText == null)
+    //    {
+    //        GameObject.Find("LivingTimer").TryGetComponent(out _TimerText);
+    //    }
+
+    //    StartCoroutine(UpdateTimer_Co());
+    //    StartCoroutine(UpdateGauge());
+    //}
 
     /// <summary>
     /// 전투 씬 진입 시 호출 메서드(전투 시작 시각 저장용)
@@ -63,16 +72,31 @@ public class UIManager : MonoBehaviour
     }
 
     /// <summary>
+    /// 방귀게이지 상시 충전 시작
+    /// </summary>
+    public void UpdateGauge()
+    {
+        _Gauge_Co = StartCoroutine(UpdateGauge_Co());
+    }
+
+    /// <summary>
     /// 방귀 게이지 UI에 표시
     /// </summary>
-    public IEnumerator UpdateGauge()
+    public IEnumerator UpdateGauge_Co()
     {
         while (true)
         {
             _TargetImage.fillAmount = _player_OBJ.CurrentGauge / _player_OBJ.MaxGauge;
             yield return null;
         }
+    }
 
+    /// <summary>
+    /// 타이머 실행
+    /// </summary>
+    public void UpdateTimer()
+    {
+        _Timer_Co = StartCoroutine(UpdateTimer_Co());
     }
 
     /// <summary>
@@ -113,5 +137,93 @@ public class UIManager : MonoBehaviour
     public void ChoosenCharacter(string characterName)
     {
         GameManager.Instance._ChooseCharacter = characterName;
+
+        TurnonVideo();
     }
+
+    /// <summary>
+    /// 스킬 쿨타임 업데이트
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator UpdateSkillColl_Co()
+    {
+        while (_player_OBJ)
+        {
+
+
+            yield return null;
+        }
+    }
+
+
+    public void TurnonVideo()
+    {
+        _VideoPlayer.GetComponent<UnityEngine.Video.VideoPlayer>().Play();
+
+        StartCoroutine(LoadingAsync("MainGame"));
+    }
+
+    /// <summary>
+    /// 동영상 종료 후 
+    /// </summary>
+    /// <param name="sceneName"></param>
+    /// <returns></returns>
+    public IEnumerator LoadingAsync(string sceneName)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        asyncOperation.allowSceneActivation = false; // 씬 로딩이 완료되는대로 씬을 활성화
+
+        while (!asyncOperation.isDone)
+        {
+            UnityEngine.Video.VideoPlayer videoPlayer;
+            if (_VideoPlayer != null && _VideoPlayer.TryGetComponent(out videoPlayer))
+            {
+                // 동영상 플레이가 끝나면 다음 씬으로 이동
+                if (videoPlayer.isPaused)
+                {
+                    CheckStartTime();
+
+                    asyncOperation.allowSceneActivation = true;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// 게임 오버 시 이름 입력하는 부분 나오게 함
+    /// </summary>
+    public void OnGameOverUI()
+    {
+        _GameOver_Canvas.gameObject.SetActive(true);
+
+    }
+
+    /// <summary>
+    /// 게임 오버 후 플레이어가 이름 입력하고 버튼 클릭
+    /// </summary>
+    public void EnterPlayerName()
+    {
+        GameManager.Instance.EnterPlayerName(_GameOver_Canvas.GetComponentInChildren<InputField>().text);
+
+    }
+
+    /// <summary>
+    /// 게임 오버 팝업에서 캐릭터 선택 씬으로 이동 버튼을 누른 경우
+    /// </summary>
+    public void GoToCharacterScene()
+    {
+        SceneManager.LoadScene("CharacterSelection");
+    }
+
+    /// <summary>
+    /// 게임 오버 팝업에서 랭킹 버튼을 누른 경우
+    /// </summary>
+    public void OnRankingUI()
+    {
+
+    }
+
+
 }
