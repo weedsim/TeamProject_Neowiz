edcbdc61760e1a6bbe537460612d1895dcf2f3f3
KINGBOB <9394jy@gmail.com>
2025-12-09 11:22:50 +0900

    게임오버랑 게임오버 수정
    ㅈㄱㄴ

--- Assets/3.Script/Stuart/Etc/RankingUI.cs
+++ Assets/3.Script/Stuart/Etc/RankingUI.cs
@@ -2,18 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class RankingUI : MonoBehaviour
 {
     public TextMeshProUGUI rankText;
-    public TextMeshProUGUI nameText;
+    public Text nameText;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI timeText;
 
     public void SetData(int rank, string name, float score, string time)
     {
-        rankText.text = rank + "deung";
+        rankText.text = rank + "DDONG";
         nameText.text = name;
         scoreText.text = score + "time";
         timeText.text = time;
